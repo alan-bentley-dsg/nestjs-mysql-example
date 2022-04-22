896db313a8ca09e2c1f553d8849673fd195cdcc9
Alan Bentley <alan.bentley@dcsg.com>
2022-04-22 16:10:19 -0400

    udpate migration to add age to User entity

--- migration/1650657656924-UserTableAddAge.ts
+++ migration/1650657656924-UserTableAddAge.ts
@@ -3,9 +3,11 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class UserTableAddAge1650657656924 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('ALTER TABLE `user` ADD age INT(11)');
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropColumn('user','age');
     }
 
 }
